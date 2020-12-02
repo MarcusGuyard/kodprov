145f8a51f0d01db957e8dbbb8326acd60fabc5a5
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-12-02 10:25:29 +0100

    Print trainer with most courses planned

--- main.py
+++ main.py
@@ -9,6 +9,32 @@ def download_course_data():
     return courses
 
 
+def print_trainer_with_most_courses(course_data):
+    # TODO (15 p): Also print out the phone number
+    # of that trainer.
+    # Note: The trainer is an employee of ProAgile,
+    # and public data about employees are available
+    # from this API endpoint:
+    #    https://proagile.se/api/publicEmployees
+
+    trainer_course_count = {}
+    for course in course_data:
+        trainer_name = course['trainerName']
+        if trainer_name not in trainer_course_count:
+            trainer_course_count[trainer_name] = 0
+        trainer_course_count[trainer_name] = trainer_course_count[trainer_name] + 1
+
+    max_course_count = max(trainer_course_count.values())
+    for trainer_name in trainer_course_count:
+        if trainer_course_count[trainer_name] == max_course_count:
+            most_courses_trainer = trainer_name
+
+    print(f"""
+Trainer with most courses is {most_courses_trainer}
+he/she has {max_course_count} courses planned in future.
+""")
+
+
 def run():
     course_data = download_course_data()
     while True:
@@ -26,25 +52,14 @@ Q) Quit
         if user_answer == '2':
             list_next_five(course_data)
         if user_answer == '3':
-            # TODO (10 p): Allow user entering only part of name.
-            # E.g. if the user enters "fredrik", all courses
-            # held by "Fredrik Wendt" will be listed.
             trainer_name = input("Name of trainer:")
-            print(f"These courses are held by {trainer_name}:")
+            print(f"Courses matching trainer {trainer_name}:")
             matching_courses = [course for course in course_data
-                                if course['trainerName'] == trainer_name]
+                                if trainer_name.lower() in course['trainerName'].lower()]
             for num, course in enumerate(matching_courses):
-                print(f"{num}. {course['courseName']} ({course['startDate']})")
+                print(f"{num}. {course['trainerName']} {course['courseName']} ({course['startDate']})")
         if user_answer == '4':
-            # TODO (10 p): Print the name of the trainer who
-            # holds most courses in the future.
-            # TODO (15 p): Also print out the phone number
-            # of that trainer.
-            # Note: The trainer is an employee of ProAgile,
-            # and public data about employees are available
-            # from this API endpoint:
-            #    https://proagile.se/api/publicEmployees
-            print('fix me')
+            print_trainer_with_most_courses(course_data)
         if user_answer.upper() == 'Q':
             print("Good-bye and thank you for the fish!")
             return
@@ -62,20 +77,10 @@ def list_trainers(courses):
 
 
 def list_next_five(course_data):
-    # TODO (10p):
-    # 1. First column is always 2 column wide
-    # 2. Second column is always 50 columns wide
-    # 3. Third column is as small as possible
-    # TODO (5p):
-    #  - We want next 5, not all courses printed
-    # TODO (5p):
-    #  - We want the sorted on startDate, not name
-    # TODO (5p):
-    #  - We want the output numbered from 1, not 0
     print("Next 5 courses sorted by date:")
-    course_data = sorted(course_data, key=lambda c: c['name'])
-    for num, course in enumerate(course_data):
-        print(f"{num}. {course['courseName']} ({course['startDate']})")
+    course_data = sorted(course_data, key=lambda c: c['startDate'])
+    for num, course in enumerate(course_data[:5], start=1):
+        print(f"{num:2}. {course['courseName']:50} ({course['startDate']})")
 
 
 if __name__ == '__main__':
