2ff781b54fb776f94109a4db9b357681a8578018
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-12-02 10:35:38 +0100

    Add docstrings

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 
 
 def download_course_data():
+    """Fetch JSON data for ProAgile future courses and convert to
+    Python list"""
     print("Downloading all future course data... ", end='')
     r = requests.get("https://proagile.se/api/publicEvents")
     courses = r.json()
@@ -9,55 +11,50 @@ def download_course_data():
     return courses
 
 
-def print_trainer_with_most_courses(course_data):
-    # TODO (15 p): Also print out the phone number
-    # of that trainer.
-    # Note: The trainer is an employee of ProAgile,
-    # and public data about employees are available
-    # from this API endpoint:
-    #    https://proagile.se/api/publicEmployees
+def download_employee_data():
+    """Fetch JSON data for ProAgile employees and convert to
+    Python list"""
+    print("Downloading all future course data... ", end='')
+    r = requests.get("https://proagile.se/api/publicEmployees")
+    employees = r.json()
+    print("Done.")
+    return employees
 
+
+def print_trainer_with_most_courses(course_data):
+    """Find the trainer with most planned courses
+       in future and print his/her name and phone number"""
     trainer_course_count = {}
     for course in course_data:
         trainer_name = course['trainerName']
         if trainer_name not in trainer_course_count:
             trainer_course_count[trainer_name] = 0
         trainer_course_count[trainer_name] = trainer_course_count[trainer_name] + 1
-
     max_course_count = max(trainer_course_count.values())
     for trainer_name in trainer_course_count:
         if trainer_course_count[trainer_name] == max_course_count:
             most_courses_trainer = trainer_name
-
-    print(f"""
-Trainer with most courses is {most_courses_trainer}
+    employees = download_employee_data()
+    for employee in employees:
+        if employee["name"] == most_courses_trainer:
+            phone_number = employee["phone"]
+    print(f"""Trainer with most courses is {most_courses_trainer}
 he/she has {max_course_count} courses planned in future.
-""")
+Phone number: {phone_number}""")
 
 
 def run():
+    """Main program"""
     course_data = download_course_data()
     while True:
-        print("""
----> MENU <---
-1) List all trainers sorted by name
-2) List 5 next upcoming courses sorted by date
-3) List all courses with a specific trainer
-4) List telephone number of most popular trainer
-Q) Quit
-""")
+        print_main_menu()
         user_answer = input("What say you? ").upper().strip()
         if user_answer == '1':
             list_trainers(course_data)
         if user_answer == '2':
             list_next_five(course_data)
         if user_answer == '3':
-            trainer_name = input("Name of trainer:")
-            print(f"Courses matching trainer {trainer_name}:")
-            matching_courses = [course for course in course_data
-                                if trainer_name.lower() in course['trainerName'].lower()]
-            for num, course in enumerate(matching_courses):
-                print(f"{num}. {course['trainerName']} {course['courseName']} ({course['startDate']})")
+            find_course_by_trainer(course_data)
         if user_answer == '4':
             print_trainer_with_most_courses(course_data)
         if user_answer.upper() == 'Q':
@@ -65,7 +62,30 @@ Q) Quit
             return
 
 
+def find_course_by_trainer(course_data):
+    """Let user search for courses using part of trainers name"""
+    trainer_name = input("Name of trainer:")
+    print(f"Courses matching trainer {trainer_name}:")
+    matching_courses = [course for course in course_data
+                        if trainer_name.lower() in course['trainerName'].lower()]
+    for num, course in enumerate(matching_courses):
+        print(f"{num}. {course['trainerName']} {course['courseName']} ({course['startDate']})")
+
+
+def print_main_menu():
+    """Print the main menu"""
+    print("""
+---> MENU <---
+1) List all trainers sorted by name
+2) List 5 next upcoming courses sorted by date
+3) List all courses with a specific trainer
+4) List telephone number of most popular trainer
+Q) Quit
+""")
+
+
 def list_trainers(courses):
+    """Print list of ProAgile trainers"""
     print("The trainers at ProAgile are:")
     trainers = []
     for course in courses:
@@ -77,6 +97,7 @@ def list_trainers(courses):
 
 
 def list_next_five(course_data):
+    """List next 5 upcoming courses"""
     print("Next 5 courses sorted by date:")
     course_data = sorted(course_data, key=lambda c: c['startDate'])
     for num, course in enumerate(course_data[:5], start=1):
